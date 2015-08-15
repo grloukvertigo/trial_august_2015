c7a9eeddc0a3bbef530fe080d1912b82a99c6ccb
grloukvertigo <grloukvertigo@gmail.com>
2015-08-15 19:34:02 +0300

    first upload

---
+++ src/minus/main.java
@@ -0,0 +1,13 @@
+package minus;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		sum s = new sum(10,9);
+		s.add(10, 9);
+		
+	}
+
+}
---
+++ src/minus/sum.java
@@ -0,0 +1,18 @@
+package minus;
+
+public class sum {
+
+	private int first;
+	private int second;
+	
+	public sum(int first, int second) {
+		super();
+		this.first = first;
+		this.second = second;
+	}
+	
+	public void add(int first,int second){
+		int result = first+second;
+		System.out.println(result);
+	}
+}
